64b5c8287d887f0516d380d8c0d530d0aa848bdc
marcioc0st <marcio@vizir.com.br>
2019-02-23 11:56:20 -0300

    steppers and slider identification as well as date component

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log('test')
-  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! test</Text>
+      <View>
+        <AddEntry />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,9 @@
-export function isBetween (num, x, y) {
+import React from 'react';
+import { View } from 'react-native';
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { white } from './colors';
+
+export function isBetween(num, x, y) {
   if (num >= x && num <= y) {
     return true
   }
@@ -6,7 +11,7 @@ export function isBetween (num, x, y) {
   return false
 }
 
-export function calculateDirection (heading) {
+export function calculateDirection(heading) {
   let direction = ''
 
   if (isBetween(heading, 0, 22.5)) {
@@ -34,8 +39,76 @@ export function calculateDirection (heading) {
   return direction
 }
 
-export function timeToString (time = Date.now()) {
+export function timeToString(time = Date.now()) {
   const date = new Date(time)
   const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
   return todayUTC.toISOString().split('T')[0]
+}
+
+
+export function getMetricMetaInfo(metric) {
+  const info = {
+    run: {
+      displayName: 'Run',
+      max: 50,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon: () => (
+        <View>
+          <MaterialIcons name="directions-run" color={'black'} size={35} />
+        </View>
+      ),
+    },
+    bike: {
+      displayName: 'Bike',
+      max: 100,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon: () => (
+        <View>
+          <MaterialCommunityIcons name="bike" color={'black'} size={35} />
+        </View>
+      ),
+    },
+    swim: {
+      displayName: 'Swim',
+      max: 9900,
+      unit: 'meters',
+      step: 100,
+      type: 'steppers',
+      getIcon: () => (
+        <View>
+          <MaterialCommunityIcons name="swim" color={'black'} size={35} />
+        </View>
+      ),
+    },
+    sleep: {
+      displayName: 'Sleep',
+      max: 24,
+      unit: 'hours',
+      step: 1,
+      type: 'slider',
+      getIcon: () => (
+        <View>
+          <FontAwesome name="bed" color={'black'} size={35} />
+        </View>
+      ),
+    },
+    eat: {
+      displayName: 'Eat',
+      max: 10,
+      unit: 'rating',
+      step: 1,
+      type: 'slider',
+      getIcon: () => (
+        <View>
+          <MaterialCommunityIcons name="food" color={'black'} size={35} />
+        </View>
+      ),
+    },
+  }
+
+  return typeof metric === 'undefined' ? info : info[metric];
 }
